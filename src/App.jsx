ed4171d618c699a1c2c0d85c4948b50bf6bab328
Sphelele <sphelelezonke901@gmail.com>
2025-03-13 13:38:32 +0200

    Update App.jsx
    NEW UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -111,20 +111,7 @@ function App() {
           </div>
           </header>
      
-           {trendingMovies.length > 0 && (
-            <section className="trending">
-              <h2>Trending Movies</h2>
-              <ul>
-                {trendingMovies.map((movie, index) => (
-                  <li key={movie.$id}>
-                       <p>{index + 1}</p>
-                       <img src={movie.poster_url} alt={movie.title ? <Spinner/> : 'Error'}/>
-                  </li>
-                ))}
-              </ul>
-            </section>
-
-           )}
+        
          
    
 
